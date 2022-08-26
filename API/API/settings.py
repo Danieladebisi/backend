5b05b35686969b5e7e3b2c12e126b8e8ab3c34a3
Adebisi Daniel <danidannyco@gmail.com>
2022-08-26 23:54:50 +0100

    o commit

--- API/API/settings.py
+++ API/API/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-mpinfp5h!wsg#71u4c-*6j-jru@$ychegz$gywa!gy#d@^ak&^
 DEBUG = False
 # DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'danform.herokuapp.com']
+ALLOWED_HOSTS = ['localhost']
 # ALLOWED_HOSTS = []
 
 
