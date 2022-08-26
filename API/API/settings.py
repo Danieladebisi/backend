bf1e9acd4089a2d6b7b43b0683d3838c68ef5439
Adebisi Daniel <danidannyco@gmail.com>
2022-08-26 23:47:51 +0100

    work na  please commit

--- API/API/settings.py
+++ API/API/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,10 +26,10 @@ SECRET_KEY = 'django-insecure-mpinfp5h!wsg#71u4c-*6j-jru@$ychegz$gywa!gy#d@^ak&^
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-# DEBUG = True
+DEBUG = True
 
-ALLOWED_HOSTS = [ 'danform.herokuapp.com']
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'danform.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -133,5 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
